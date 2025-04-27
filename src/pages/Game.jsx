98ead00d9ea192e9bf18c935fb75b83330005b53
ArnaudMegaDude <arnauboy@gmail.com>
2025-04-27 02:24:12 +0200

    vibrate when wrong guess

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -73,6 +73,9 @@ export default function Game() {
         setGuessIsCorrect(true)
       } else {
         setGuessIsCorrect(false)
+        if (navigator.vibrate) {
+          navigator.vibrate(200)
+        }
       }
     }
   }
